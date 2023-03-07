1852fd54dec39fe65d01085d160cb91efee1885f
decidev22 <hamento22@gmail.com>
2023-03-07 15:42:33 +1300

    Loading Screen prototype version

--- src/components/Loading_Screen.tsx
+++ src/components/Loading_Screen.tsx
@@ -1,8 +1,11 @@
+import { motion } from "framer-motion";
 interface Props {}
 const Loading_Screen: React.FC<Props> = () => {
   return (
-    <div className="w-full min-h-height bg-indigo-800">
-      <span className="font-poppin text-lg">Andy Baeck's Tech Blog Intro</span>
+    <div className="w-full h-screen bg-primary grid place-items-center">
+      <motion.span className="font-poppin text-white">
+        Andy Baeck's Tech Blog Intro
+      </motion.span>
     </div>
   );
 };
