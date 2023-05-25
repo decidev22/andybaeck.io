975aa82db28743e31cbe3ab58e78acde7e26282b
decidev22 <hamento22@gmail.com>
2023-05-25 16:40:26 +1200

    Responsiveness update font size and logo size

--- src/components/Loading_Screen.tsx
+++ src/components/Loading_Screen.tsx
@@ -13,16 +13,16 @@ const Loading_Screen: React.FC<Props> = () => {
           alt="logo"
           animate={{ rotateY: 360 }}
           transition={{ duration: 2, repeat: Infinity }}
-          className="w-[65px] h-[65px] mt-7 mr-2"
+          className="w-[30px] h-[30px] mt-4 mr-2 xs:w-[65px] xs:h-[65px] md:w-[100px] md:h-[100px] md:mr-5 lg:w-[120px] lg:h-[120px] lg:mr-5"
         />
-        <div className="font-poppins font-bold text-white xs:text-[40px] md:text-[65px]">
+        <div className="font-poppins font-bold text-white text-[23px] xs:text-[45px] md:text-[90px] lg:text-[120px]">
           <Title_Array />
         </div>
       </div>
       <div className="flex flex-col text-white ">
         {" "}
         <div className="flex justify-center">
-          Hover over my blog title while you wait
+          Hover/Click over my blog title while you wait
         </div>
         <div className="flex justify-center">Loading . . .</div>
       </div>
